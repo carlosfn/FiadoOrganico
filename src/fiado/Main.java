23a21bf3bb402706df2f52278dc5ed9e43454102
carlosfn <82351682+carlosfn@users.noreply.github.com>
2021-04-12 02:13:18 -0300

    Add files via upload

---
+++ src/fiado/Main.java
@@ -0,0 +1,65 @@
+package fiado;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] fiados = new int[10];
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Digite o nome do cliente: ");
+		String nome = scanner.nextLine();
+
+		System.out.print("Digite o telefone do cliente: ");
+		String telefone = scanner.nextLine();
+
+		Consumidor consumidor = new Consumidor(nome, telefone);
+
+		for (int i = 0; i < fiados.length; i++) {
+			System.out.print("Digite o valor da compra: ");
+			int fiado = Integer.valueOf(scanner.nextLine());
+			consumidor.registrarFiado(fiado);
+		}
+
+		int total = consumidor.getFiado();
+
+		if (total > 100) {
+			System.out.println("Cliente: " + consumidor.getNome() + " está devendo: R$ " + total + ",00 >> contatos pelo telefone : "
+					+ consumidor.getTelefone());
+		} else {
+			System.out.println("Cliente: " + consumidor.getNome() + " está devendo: R$ " + total + ",00 >> contatos pelo telefone : "
+					+ consumidor.getTelefone());
+		}
+
+	}
+
+	public static int somaWhile(int[] fiados) {
+		int i = 0;
+		int total = 0;
+		while (i < fiados.length) {
+			total += fiados[i];
+			i++;
+		}
+		return total;
+	}
+
+	public static int somaFor(int[] fiados) {
+		int total = 0;
+		for (int i = 0; i < fiados.length; i++) {
+			total += fiados[i];
+		}
+		return total;
+	}
+
+	public static int somaForeach(int[] fiados) {
+		int total = 0;
+		for (int fiado : fiados) {
+			total += fiado;
+		}
+		return total;
+	}
+
+}
